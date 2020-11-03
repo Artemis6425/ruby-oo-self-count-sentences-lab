a7920172fc005a0f4ade2ad3baf450115385789c
Artemis6425 <aurorarose6425@gmail.com>
2020-11-03 15:04:29 -0600

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,32 @@ require 'pry'
 class String
 
   def sentence?
-    
+    if self.end_with?(".")
+      return true
+    else
+      return false
+    end
   end
 
   def question?
-
+    if self.end_with?("?")
+      return true
+    else
+      return false
+    end
   end
 
   def exclamation?
-
+    if self.end_with?("!")
+      return true
+    else 
+      return false
+    end
   end
 
   def count_sentences
-
+    temp = self.split(/[?!.]/)
+    temp.reject!{|x| x.empty?}
+    return temp.length
   end
 end
